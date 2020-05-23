1c97701ee06c9e00a804964a9eb595c4c1054ebe
Thắng Nguyễn <41680135+thangnguyen24896@users.noreply.github.com>
2020-05-23 18:20:39 +0700

    Assignment8

---
+++ Assignment 8/assignment8_1_1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int n, *p;
+	printf("Nhap n = ");
+	scanf("%d", &n);
+	p = (int *)malloc(n*sizeof(int));
+	for(int i=0;i<n;i++){
+		printf("Nhap pt thu %d: ",i);
+		scanf("%d", p+i);
+	}
+	int max = 0,count = 0;
+	for(int i=0;i<n;i++){
+		if(*(p+i) > 0){
+			count++;
+			if(count > max) max = count;
+		}else{
+			count = 0;
+		}
+	}
+	printf("So luong do duong lien tiep lon nhat: %d",max);
+}
---
+++ Assignment 8/assignment8_2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	int n;	
+	printf("Nhap vao so phan tu cho mang n = ");
+	scanf("%d", &n);
+	int ary[n];
+	
+	for(int i=0;i<n;i++){
+		printf("Nhap vao phan tu thu %d: ", i);
+		scanf("%d", &ary[i]);
+	}
+	
+	int max = 0, count = 0;
+	for(int i=0;i<n;i++){
+		if(ary[i] > 0){
+			count+=ary[i];
+			if(count > max) max = count;
+		}else{
+			count = 0;
+		}
+	}
+	printf("Tong luong so duong lien tiep nhieu nhat: %d\n",max);
+}
